fa07cd31e93def3a93cd96c344143c3214c0da09
rak <sonwonrak@gmail.com>
2019-08-06 23:58:45 +0900

    네 번째 과제 - 피보나치 수열 만들기

---
+++ python_04.py
@@ -0,0 +1,25 @@
+'''
+
+### Python Algorithm Study ###
+# author  : laziness
+# date    : 2019/08/06
+
+네 번째 과제
+::피보나치 수열 만들기::
+
+x가 0 또는 1일 경우는 x값을 그대로 리턴한다.
+x가 2이상일 경우 Fn = Fn-1 + fn-2의 수식이 반복된다.
+
+
+'''
+# Test Case
+test01 = [1,2,3,4,5]
+test02 = [55]
+test03 = [1,2,3,4,5,6,7,8,22,44,66,321]
+
+def solution(x):
+    answer = 0
+    a
+    bin
+    c
+    return answer
