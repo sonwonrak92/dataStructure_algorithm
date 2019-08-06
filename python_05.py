bd3d944a9d485c649927fdd5fffc8dd36fe67229
rak <sonwonrak@gmail.com>
2019-08-07 00:01:06 +0900

    다섯 번째 과제 - 재귀함수를 이용하여, 이진탐색 구현하기

--- python_05.py
+++ python_05.py
@@ -15,7 +15,7 @@ test02 = [55]
 test03 = [1,2,3,4,5,6,7,8,22,44,66,321]
 
 def solution(L, x, l, u):
-    if L.count(x) > 0 :
+    if L.count(x) < 0 :
         return -1
     mid = (l + u) // 2
     if x == L[mid]:
