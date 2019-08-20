f5017bf9b85068515d3b62b69399f9e5db07f801
rak <sonwonrak@gmail.com>
2019-08-20 22:20:12 +0900

    챕터3 - 양방향 연결 리스트로 구현하는 큐

---
+++ PYTHON_03/python_03_02.py
@@ -0,0 +1,52 @@
+'''
+
+### Python Algorithm Study ###
+# author  : laziness
+# date    : 2019/08/20
+
+챕터3 - 두 번째 과제
+::중위표현 수식 --> 후위표현 수식::
+
+'''
+class ArrayStack:
+    
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+
+def solution(expr):
+    match = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    S = ArrayStack()
+    for c in expr:
+        if c in '({[':  #괄호가 나올 경우 연산자스택에 넣는다.          
+            S.push(c)
+
+        elif c in match:    #닫는 괄호가 나올 경우에는 여는 괄호를 담는 스택이 비어있는지 확인한다.
+            if S.isEmpty() : #비어있다면 여는 괄호가 없이 닫는 괄호만 존재한다는 것이므로 리턴한다.
+                return False
+            else:                
+                t = S.pop() 
+                if t != match[c]: #닫는 괄호끼리 짝이 안맞는것이 존재하면 리턴한다.
+                    return False
+
+    return S.isEmpty()
---
+++ PYTHON_03/python_03_03.py
@@ -0,0 +1,77 @@
+'''
+
+### Python Algorithm Study ###
+# author  : laziness
+# date    : 2019/08/20
+
+챕터3 - 세 번째 과제
+::후위표현 수식 계산::
+
+'''
+class ArrayStack:
+    
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+
+def splitTokens(exprStr):
+    tokens = []
+    val = 0
+    valProcessing = False
+    for c in exprStr:
+        if c == ' ':
+            continue
+        if c in '0123456789':
+            val = val * 10 + int(c)
+            valProcessing = True
+        else:
+            if valProcessing:
+                tokens.append(val)
+                val = 0
+            valProcessing = False
+            tokens.append(c)
+    if valProcessing:
+        tokens.append(val)
+
+    return tokens
+
+
+def infixToPostfix(tokenList):
+    prec = {
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+        '(': 1,
+    }
+
+    opStack = ArrayStack()
+    postfixList = []
+
+    return postfixList
+
+
+def postfixEval(tokenList):
+    pass
+
+
+def solution(expr):
+    tokens = splitTokens(expr)
+    postfix = infixToPostfix(tokens)
+    val = postfixEval(postfix)
+    return val
